aa29adf890281fdd668b78591b263f1ae946a3cd
Jose <josetovarc2@gmail.com>
2023-05-02 23:45:09 -0500

    :code

--- database/config.py
+++ database/config.py
@@ -2,13 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:temporal123@localhost/social_app"
+# DATABASE_URL = "postgresql://postgres:temporal123@localhost/social_app"
+DATABASE_URL = "postgresql://postgres:567322963@socialapp.coldyyfud5g4.us-east-1.rds.amazonaws.com:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 
 db_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 def get_db():
     db = db_session()
